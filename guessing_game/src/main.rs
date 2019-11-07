5bbedba89f13e6bdb4a8cde5fc0e83b607a1671d
Owen Sullivan <multiojuice@gmail.com>
2019-11-07 12:14:00 -0500

    Added better error handling and fixed loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,9 +4,9 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
-        let secret_number = rand::thread_rng().gen_range(1, 101);
         println!("Please input your guess.");
 
         let mut guess = String::new();
@@ -14,8 +14,13 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line... L");
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!!!"); 
+                continue;
+            },
+        };
 
         println!("You guessed {}", guess);
 
